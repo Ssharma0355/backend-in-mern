c9164d44b4d5a3d4dba44867403625f7309a79ff
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-09 10:51:16 +0530

    creating routes

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get("/",(req,res)=>{
     res.send("Hi Sachin app is running on port 8000")
 })
 
+app.get("/about",(req,res)=>{
+    res.send("This is about page")
+})
+
+app.get("/sachin",(req,res)=>{
+    res.send("Sachin is here")
+})
+
 app.listen(8000,()=>{
     console.log("App is running on 8000")
 })
+
