6d9c79404770e800e49119d1fbb73afbeb7feffb
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-10 09:17:58 +0530

    creating notes app

--- server.js
+++ server.js
@@ -1,36 +1,5 @@
-const express = require("express")
-const cors = require("cors")
+const app = require("./src/app")
 
-const app = express(); // server is created
-
-const people =[{
-    name:"Sachin",
-    email:"ssharmagmail.com",
-    phone:"911370488"
-},
-{
-    name:"Sachin",
-    email:"ssharmagmail.com",
-    phone:"911370488"
-},
-]
-
-app.use(cors())
-
-app.get("/",(req,res)=>{
-    res.send(people)
+app.listen(3000,()=>{
+    console.log("running")
 })
-
-app.get("/about",(req,res)=>{
-    res.send("This is about page")
-})
-
-app.get("/sachin",(req,res)=>{
-    res.send("Sachin is here")
-})
-
-// port number 8000
-app.listen(8000,()=>{
-    console.log("App is running on 8000") // server is started here
-})
-
