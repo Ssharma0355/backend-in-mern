5d71209f9b3d387df5c91543fadb706840071cdd
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-11 09:56:04 +0530

    get all notes

--- src/app.js
+++ src/app.js
@@ -4,10 +4,18 @@ const app = express()
 app.use(express.json()) // any request body data to convernt into readable for express 
 
 const notes =[];
+
+// post notes
 app.post("/notes",(req,res)=>{
     notes.push(req.body)
     res.status(201).json({
         message:"Notes Created successfully"
+    })
+})
+// get all notes
+app.get("/getAllNotes",(req,res)=>{
+    res.status(200).json({
+        notes:notes
     })
 })
 
