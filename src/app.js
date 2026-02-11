e3a33a2cdf60f60acfd1c2465d25bb43463919bc
Sachin Sharma <sachinsharma@Sachins-MacBook-Air.local>
2026-02-11 09:43:52 +0530

    middle ware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json()) // any request body data to convernt into readable for express 
+
+app.post("/notes",(req,res)=>{
+    console.log(req.body)
+    res.send("Runnningg",req.body)
+})
 
 
 module.exports = app;
